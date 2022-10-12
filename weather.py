10d897942f1cdf217245fcd0f6a8bea19bfe81d2
Jessica Sandres <jsandres394@gmail.com>
2022-10-12 09:05:35 -0700

    changes for commit

--- weather.py
+++ weather.py
@@ -59,30 +59,30 @@ def report_daily(data, date):
         if date == key[0:8]:
             m = calendar.month_name[int(date[4:6])] + " " + str(int(date[6:8])) + ", " + str(int(date[0:4]))
             tm = key[8:10] + ":" + key[10:12] + ":" + key[12:14]
-            t = date[key]['t']
-            h = date[key]['h']
-            r = date[key]['r']
+            t = data[key]['t']
+            h = data[key]['h']
+            r = data[key]['r']
 
             display += f'{m:22}{tm:8}{t:13}{h:10}{r:10.2f}' + '\n'
     return display
 
 def report_historical(data):
     display = "============================== HISTORICAL REPORT ===========================\n"
-    display += "			              Minimum      Maximum   Minumum   Maximum     Total\n"
+    display += "                          Minimum      Maximum   Minumum   Maximum     Total\n"
     display += "Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n"
     display += "====================  ===========  ===========  ========  ========  ========\n"
-    h = ''
+    h = ""
     for key in data:
         if h == key[0:8]:
             continue
         else:
-            h == key[0:8]
-            m = calendar.month_name[int(h[4:6])] + " " + str(int(h[6:8])) + "," + str(int(h[0:4]))
-            min_temp = min_temperature(data, h)
-            max_temp = max_temperature(data, h)
-            min_hum = min_humidity(data, h)
-            max_hum = max_humidity(data, h)
-            rain = tot_rain(data, h)
+            h = key[0:8]
+            m = calendar.month_name[int(h[4:6])] + " " + str(int(h[6:8])) + ", " + str(int(h[0:4]))
+            min_temp = min_temperature(data = data, date = h)
+            max_temp = max_temperature(data = data, date = h)
+            min_hum = min_humidity(data = data, date = h)
+            max_hum = max_humidity(data = data, date = h)
+            rain = tot_rain(data = data, date = h)
 
-            display += f'{m:22}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:20:2f}' + '\n'
+            display += f'{m:20}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:10.2f}' + '\n'
     return display
